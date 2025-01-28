a0e61ce6215689915af2d36e460c589d97f85478
Kaique Prado <kiqprado@outlook.com>
2025-01-28 14:11:34 -0300

    feat: add participants on create trip.

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -16,11 +16,18 @@ export async function createTrip(app: FastifyInstance) {
         starts_at: z.coerce.date(),
         ends_at: z.coerce.date(),
         owner_name: z.string(),
-        owner_email: z.string().email()
+        owner_email: z.string().email(),
+        emails_to_invite: z.array(z.string().email())
       })
     }
   }, async (req) => {
-    const { destination, starts_at, ends_at, owner_name, owner_email } = req.body
+    const { 
+      destination, 
+      starts_at, 
+      ends_at, 
+      owner_name, 
+      owner_email, 
+      emails_to_invite } = req.body
 
     if(dayjs(starts_at).isBefore(new Date())) {
       throw new Error ("Invalid Trip starts date.")
@@ -34,12 +41,28 @@ export async function createTrip(app: FastifyInstance) {
       data: {
         destination,
         starts_at,
-        ends_at
+        ends_at,
+        participants: {
+          createMany: {
+            data: [{
+              name: owner_email,
+              email: owner_email,
+              is_owner: true,
+              is_confirmed: true,
+            },
+            ...emails_to_invite.map(email => {
+              return { email }
+            })
+          ]
+          }
+        }
       }
     })
 
     const formattedStartDate = dayjs(starts_at).format('LL')
     const formattedEndDate = dayjs(ends_at).format('LL')
+
+    const confirmationLink = `http://localhost:3333/trips/${trip.id}/confirm`
 
     const mail = await getMailClient()
 
@@ -52,14 +75,14 @@ export async function createTrip(app: FastifyInstance) {
         name: owner_name,
         address: owner_email
       },
-      subject: 'Confirme a sua viagem para ${destination} em ${formattedStartDate}',
+      subject: `Confirme a sua viagem para ${destination} em ${formattedStartDate}`,
       html: `
         <div style="font-family: sans-serif; font-size: 16px; line-height: 1.6;">
           <p>Você solicitou a criação de uma viagem para <strong>${destination}</strong> nas datas <strong>${formattedStartDate}</strong> até <strong>${formattedEndDate}</strong>.</p>
           <p></p>
           <p>Para confirmar sua viagem, clique no link abaixo:</p>
           <p></p>
-          <a href="">Confirmar viagem</a>
+          <a href="${confirmationLink}">Confirmar viagem</a>
           <p></p>
           <p>Caso esteja usando o dispositivo móvel, você também pode confirmar a criação da viagem pelos aplicativos:</p>
           <p></p>
