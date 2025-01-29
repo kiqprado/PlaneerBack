a7fee64757b7b5436107b5006fb9ef217564e5e7
Kaique Prado <kiqprado@outlook.com>
2025-01-29 12:23:40 -0300

    feat: Updating attendance confirmation for trip participants

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,21 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 import { validatorCompiler, serializerCompiler } from 'fastify-type-provider-zod'
 
 import { createTrip } from './routes/create-trip'
+import { confirmTrip } from './routes/confirm-trip'
 
 const app = fastify()
+
+app.register(cors, {
+  origin: true
+})
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
 app.register(createTrip)
+app.register(confirmTrip)
 
 app.listen({
   port: 3333
