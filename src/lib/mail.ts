8c64725de29130267a9b40edb71b4a3defa34006
Kaique Prado <kiqprado@outlook.com>
2025-02-03 14:24:19 -0300

    fix:  fixing a issue on get Mail to participant

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -1,15 +1,15 @@
 import nodemailer from 'nodemailer'
 
 export async function getMailClient() {
-  const account = nodemailer.createTestAccount()
+  const account = await nodemailer.createTestAccount()
 
   const transporter = nodemailer.createTransport({
     host: 'smtp.ethereal.email',
     port: 587,
     secure: false,
     auth: {
-      user: (await account).user,
-      pass: (await account).pass
+      user: account.user,
+      pass: account.pass
     }
   })
 
