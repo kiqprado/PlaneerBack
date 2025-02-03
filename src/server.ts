1d311f533dccd47336104c2361ed53222784866f
Kaique Prado <kiqprado@outlook.com>
2025-02-03 12:29:08 -0300

    feat: creating invites to a single participant on trip.

--- src/server.ts
+++ src/server.ts
@@ -5,10 +5,13 @@ import { validatorCompiler, serializerCompiler } from 'fastify-type-provider-zod
 import { createTrip } from './routes/create-trip'
 import { confirmTrip } from './routes/confirm-trip'
 import { confirmParticipants } from './routes/confirm-participant'
+import { getParticipants } from './routes/get-participants'
 import { createActivity } from './routes/create-activity'
 import { getActivities } from './routes/get-activities'
 import { createLink } from './routes/create-link'
 import { getLinks } from './routes/get-links'
+import { createInvite } from './routes/create-invite'
+
 
 const app = fastify()
 
@@ -22,6 +25,8 @@ app.setSerializerCompiler(serializerCompiler)
 app.register(createTrip)
 app.register(confirmTrip)
 app.register(confirmParticipants)
+app.register(getParticipants)
+app.register(createInvite)
 app.register(createActivity)
 app.register(getActivities)
 app.register(createLink)
